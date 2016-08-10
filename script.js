c55868487a8317499e6ce3230139b3dc7a7f0d4d
Mkilg <marie@kilg.de>
2016-08-10 20:36:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hallo, ich bin MKII, der persönliche Bot von Marie. Schreib etwas!')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`Das verstehe ich nicht.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
